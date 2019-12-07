fd194e4c741cc8d46cc72d4fe871d651c3334b95
Jon Hinks <projects.ws@jondh.me.uk>
2019-12-07 22:54:38 +0000

    Get Jest in a running state

---
+++ bin/test-compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy all source.js files as _source.js
+for f in $(find functions/ -name source.js); do cp -- "$f" "$(dirname $f)/_$(basename $f)"; done
+
+# Search and replace in all _source.js files
+for f in $(find functions/ -name _source.js); do sed -i -e 's/exports =/module.exports =/g' $f; done
