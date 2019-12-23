6bf7664c0efe870d4ce29e8a838089b8e3c9deda
Jon Hinks <projects.ws@jondh.me.uk>
2019-12-23 00:17:04 +0000

    Add some passing Mongo tests

--- functions/getNextQuery/getNextQuery.test.js
+++ functions/getNextQuery/getNextQuery.test.js
@@ -37,6 +37,45 @@ describe('insert', () => {
         expect(await getNextQuery()).toBe(null);
     });
 
+    it('shows query that has not been run will be run', async () => {
+        global.context = getStitchContext();
+
+        // Insert a query that has never been run
+        let queries = db.collection('queries');
+        await queries.insertOne(createQueryDoc(null));
+
+        // We should get a query this time
+        let queryObject = await getNextQuery();
+        expect(queryObject.phrase).toBe('hello');
+    });
+
+    it('shows query that has not been run will be run', async () => {
+        global.context = getStitchContext();
+
+        // Calculate an "old" time
+        let lastRunAt = new Date();
+        lastRunAt.setTime(
+            lastRunAt.getTime() - (1000 * 60 * 60 * 5)
+        );
+
+        // Insert a query that needs to be run
+        let queries = db.collection('queries');
+        await queries.insertOne(createQueryDoc(lastRunAt));
+
+        // We should get a query this time
+        let queryObject = await getNextQuery();
+        expect(queryObject.phrase).toBe('hello');
+    });
+
+    function createQueryDoc(lastRunAt) {
+        return {
+            "user_id": 1,
+            "phrase": 'hello',
+            "last_run_at": lastRunAt,
+            "enabled": true
+        }
+    }
+
     function getStitchContext() {
         return {
             services: {
