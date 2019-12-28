a6b150405f3d696ea6366b9dd5eee12b1ea5103f
Jon Hinks <projects.ws@jondh.me.uk>
2019-12-28 14:56:22 +0000

    Add dummy test code

---
+++ functions/markQueryAsRun/markQueryAsRun.test.js
@@ -0,0 +1,8 @@
+const markQueryAsRun = require('./_source');
+
+describe('Some tests for markQueryAsRun', () => {
+    test('Dummy test', () => {
+        // Dummy test for now
+        expect(1).toBe(1);
+    });
+});
