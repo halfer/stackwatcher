d4d2467b6a9289f27872932929691e605b2194f2
Jon Hinks <projects.ws@jondh.me.uk>
2019-12-28 19:59:05 +0000

    Sketch out db test structure for markQueryAsRun

--- functions/markQueryAsRun/markQueryAsRun.test.js
+++ functions/markQueryAsRun/markQueryAsRun.test.js
@@ -1,7 +1,50 @@
+const {MongoClient} = require('mongodb');
 const markQueryAsRun = require('./_source');
 
 describe('Some tests for markQueryAsRun', () => {
-    test('Dummy test', () => {
+    let connection;
+    let db;
+
+    // @todo This is copied from `getNextQuery.test.js`, can we DRY up?
+    beforeAll(async () => {
+        // See this URL for unified topology: https://stackoverflow.com/a/57899638
+        connection = await MongoClient.connect(global.__MONGO_URI__, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        db = await connection.db(global.__MONGO_DB_NAME__);
+    });
+
+    // @todo This is copied from `getNextQuery.test.js`, can we DRY up?
+    afterAll(async () => {
+        await connection.close();
+        await db.close();
+    });
+
+    beforeEach(async () => {
+        await db.collection('queries').deleteMany({});
+    });
+
+    test('Successful mark query as run', () => {
         // @todo Add expectations here
     });
+
+    test('Fail if the supplied ID does not exist', () => {
+        // @todo Add expectations here
+    });
+
+    // @todo This is copied from `getNextQuery.test.js`, can we DRY up?
+    function getStitchContext() {
+        return {
+            services: {
+                get: function(serviceName) {
+                    return {
+                        db: function(databaseName) {
+                            return db;
+                        }
+                    }
+                }
+            }
+        };
+    }
 });
--- functions/markQueryAsRun/source.js
+++ functions/markQueryAsRun/source.js
@@ -1,6 +1,17 @@
 exports = async function(queryId) {
     const database = context.services.get("mongodb-atlas").db("stack_watcher");
     const queriesCollection = database.collection("queries");
+    const log = {
+        type: 'RUN',
+        time: new Date()
+    };
 
-    // FIXME Lookup & write here
+    queriesCollection.update(
+        { id: new ObjectId(queryId) },
+        {
+            $push: {
+                logs: newLog
+            }
+        }
+    )
 };
